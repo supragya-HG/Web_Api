1a6cc39663e5eb63de1eca312415e34d5b58e6ea
supragya-Hg <supragya.shrestha@housing.com>
2021-07-05 13:44:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const listItems_1 = document.querySelector('.list-items-1');
 const listItems_2 = document.querySelector('.list-items-2');
 
 var promise = new Promise(function(resolve, reject) {
-    console.log("promise start");
+    // console.log("promise start");
     const objScript = document.createElement("script");
     objScript.src = 'object.js';
     document.body.append(objScript);
@@ -34,15 +34,15 @@ var promise = new Promise(function(resolve, reject) {
     funScript.onerror = () => {
         reject(2);
     };
-    console.log("promise ends");
+    // console.log("promise ends");
     resolve(0);
-    console.log("promise resolved");
+    // console.log("promise resolved");
     
   });
 
 promise.then((res) => console.log("Script Loading Success")).catch((err) => console.log(err, "Script Loading Failed"));  
 
-console.log("check");
+// console.log("check");
 
 
 
